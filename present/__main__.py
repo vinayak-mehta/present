df0faeb222bd8704413802c97228986633b65b01
Vinayak Mehta <vmehta94@gmail.com>
2020-08-18 17:41:58 +0530

    Add main

---
+++ present/__main__.py
@@ -0,0 +1,16 @@
+# -*- coding: utf-8 -*-
+
+from __future__ import absolute_import
+
+
+__all__ = ("main",)
+
+
+def main():
+    from present.cli import cli
+
+    cli()
+
+
+if __name__ == "__main__":
+    main()
