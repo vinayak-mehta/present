6b910b43316a4faee40dfb5531fd01a25517b74b
Vinayak Mehta <vmehta94@gmail.com>
2020-08-20 02:26:46 +0530

    Bump version and update HISTORY.md

---
+++ HISTORY.md
@@ -0,0 +1,10 @@
+Release History
+===============
+
+master
+------
+
+0.2.0 (2020-08-20)
+------------------
+
+* First working release!
--- present/__version__.py
+++ present/__version__.py
@@ -1,6 +1,6 @@
 # -*- coding: utf-8 -*-
 
-VERSION = (0, 1, 1)
+VERSION = (0, 2, 0)
 PRERELEASE = None  # alpha, beta or rc
 REVISION = None
 
