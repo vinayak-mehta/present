c6041b8f4a146d397f7f7a52b6f07901a21f6ec9
Brooks <brooks.johnson@pm.me>
2021-09-06 19:45:38 -0700

    Refactor has_code to be based on code_blocks
    I tested this change by putting a fireworks effect on the slide and
    checking to see if I got the ValueError.

--- present/slide.py
+++ present/slide.py
@@ -404,13 +404,12 @@ class Slide(object):
         self.has_style = False
         self.has_effect = False
         self.has_image = False
-        self.has_code = False
         self.has_codio = False
         self.style = {}
         self.effect = None
         self.fg_color = 0
         self.bg_color = 7
-        self.code_blocks = []
+        self.code_blocks = None
 
         _elements = []
 
@@ -427,7 +426,8 @@ class Slide(object):
                 self.has_image = True
 
             if e.type == "code":
-                self.has_code = True
+                # This list will get built by the Slideshow
+                self.code_blocks = []
 
             if e.type == "codio":
                 self.has_codio = True
@@ -470,6 +470,10 @@ class Slide(object):
         for e in self.elements:
             e.fg = self.fg_color
             e.bg = self.bg_color
+
+    @property
+    def has_code(self):
+        return type(self.code_blocks) is list
 
     def __repr__(self):
         return f"<Slide elements={self.elements} has_style={self.has_style} has_code={self.has_code} fg_color={self.fg_color} bg_color={self.bg_color}>"
